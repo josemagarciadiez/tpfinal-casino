9027dfddd65522507d346c883ddcb8de2290ab9b
Jose Garcia <jmagarciadiez@gmail.com>
2024-11-11 20:58:29 -0300

    Example

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log("Aca consegui iconitos para el tragamoneda jajaja");
+console.log(["🍒", "🍋", "🔔", "⭐", "🍉"]);
+
+export interface IJuego {
+  iniciar(): void; // Metodo para iniciar la logica del juego
+  // y no se me ocurre nada por ahora ajajaj
+}
