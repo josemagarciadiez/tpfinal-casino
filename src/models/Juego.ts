ecd5e2175671a33177fbec8d812b8fc17ded06f1
Jose Garcia <jmagarciadiez@gmail.com>
2024-11-24 12:18:08 -0300

    Se creo esctructura para la clase abstracta Juego

---
+++ src/models/Juego.ts
@@ -0,0 +1,16 @@
+import { IJuego } from "./IJuego";
+import { Jugador } from "./Jugador";
+
+export abstract class Juego implements IJuego {
+  protected instrucciones!: string;
+
+  obtenerInstrucciones(): string {
+    return this.instrucciones;
+  }
+
+  abstract ejecutar(jugador: Jugador): {
+    apuestaTotal: number;
+    resultado: "victoria" | "derrota";
+    ganancia?: number;
+  };
+}
