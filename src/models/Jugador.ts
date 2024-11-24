8a83f818c8d50d576fc9b4131c3f5aef05fcd17c
Jose Garcia <jmagarciadiez@gmail.com>
2024-11-24 12:17:51 -0300

    Se creo estructura para la clase Jugador

---
+++ src/models/Jugador.ts
@@ -0,0 +1,15 @@
+export class Jugador {
+  private nombre: string;
+  private saldo: number;
+
+  constructor(nombre: string, saldoInicial: number) {
+    this.nombre = nombre;
+    this.saldo = saldoInicial;
+  }
+
+  // HACER:
+  // 1. Método getter de nombre
+  // 2. Método getter de saldo actual
+  // 3. Método setter para agregar saldo
+  // 4. Método setter para disminuir saldo
+}
