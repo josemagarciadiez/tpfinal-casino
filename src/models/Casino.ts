ea979b2eba2e1e014ee0c0c069502ecbb5702755
Jose Garcia <jmagarciadiez@gmail.com>
2024-11-24 13:05:52 -0300

    Se registra implementacion del nuevo Juego en la clase Casino

--- src/models/Casino.ts
+++ src/models/Casino.ts
@@ -6,6 +6,7 @@ import { Jugador } from "./Jugador";
 import { Menu } from "../utils/Menu";
 
 // Implementaciones de juegos
+import { Dados } from "../games/Dados";
 
 export class Casino {
   private jugador: Jugador | undefined;
@@ -103,7 +104,7 @@ export class Casino {
 
     switch (opcionSeleccionada) {
       case "dados":
-        // this.juego = new BlackjackDados();
+        this.juego = new Dados();
         break;
       default:
         console.log("El juego seleccionado aún no esta disponible 😢");
